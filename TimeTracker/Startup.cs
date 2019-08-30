96e0cc99084afafbfe3bc04946a955097f74711b
Jesse <dum3zeye@gmail.com>
2019-08-30 18:44:25 -0500

    Removed dead code

--- TimeTracker/Startup.cs
+++ TimeTracker/Startup.cs
@@ -38,9 +38,6 @@ namespace TimeTracker
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddDefaultIdentity<IdentityUser>()
-            //    .AddDefaultUI(UIFramework.Bootstrap4)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.Configure<IdentityOptions>(options =>
             {
