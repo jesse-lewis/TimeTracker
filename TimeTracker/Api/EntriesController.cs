37b4d56ba0894a00bc24cee10708d61795b344b3
Jesse <dum3zeye@gmail.com>
2019-09-07 11:51:15 -0500

    Added entries controller for getting entries.  Replaced ASP.NET in Entries/Index with javascript.

---
+++ TimeTracker/Api/EntriesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Data;
+using TimeTracker.Models;
+
+namespace TimeTracker.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntriesController : ControllerBase
+    {
+        private readonly IApplicationDbContext _context;
+
+        public EntriesController(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<TimeSheetEntry>> GetEntries()
+        {
+            return await _context.GetEntriesForUser(GetUserGuid());
+        }
+
+        private string GetUserGuid()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
